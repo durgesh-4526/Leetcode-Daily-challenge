d0e246f04387fb55cea5795e5979868922ba73ba
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2022-05-31 20:25:37 +0530

    Create 31_may_1461.cpp

---
+++ 31_may_1461.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        unordered_map<string,int> m;
+      for(int i = 0;i+k-1<s.size();i++){
+        string a = s.substr(i,k);
+        if(m.find(a) == m.end())m[a]++;
+        if(m.size() == pow(2,k))return true;
+      }return false;
+    }
+};
