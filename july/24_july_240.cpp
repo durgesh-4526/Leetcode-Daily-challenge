b256c42fbd4e2557eee6dcd63d40f082ffc5ab7c
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2022-07-24 06:57:55 +0530

    Create 24_july_240.cpp

---
+++ july/24_july_240.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        bool searchMatrix(vector<vector < int>> &matrix, int target)
+        {
+            int row = matrix.size();
+            int col = matrix[0].size();
+            int i = 0;
+            int j = col - 1;
+            while (i < row && j >= 0)
+            {
+                int temp = matrix[i][j];
+                if (temp == target)
+                {
+                    return true;
+                }
+                if (temp < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return false;
+        }
+};
